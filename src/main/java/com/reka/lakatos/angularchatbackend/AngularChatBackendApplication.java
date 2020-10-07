f5836482bbf59d3c7d3cd1eceda59e76be6e82cb
Melange93 <lakatos.reka.veronika@gmail.com>
2020-10-07 13:57:23 +0200

    first commit

---
+++ src/main/java/com/reka/lakatos/angularchatbackend/AngularChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reka.lakatos.angularchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularChatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reka/lakatos/angularchatbackend/AngularChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reka.lakatos.angularchatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
